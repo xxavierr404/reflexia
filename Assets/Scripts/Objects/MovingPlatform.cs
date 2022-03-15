ddea7a375ddfd9ef0c267242f602e860c2a0681b
Victor Vasiliev <vasilievvict1@yandex.ru>
2022-03-15 21:26:36 +0300

    Moving platforms now move player

--- Assets/Scripts/Objects/MovingPlatform.cs
+++ Assets/Scripts/Objects/MovingPlatform.cs
@@ -41,4 +41,14 @@ public class MovingPlatform : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) collision.transform.SetParent(null);
+    }
+
 }
