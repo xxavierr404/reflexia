8d091687f2fd0cf429802f6dd7be5a6be2845f3c
Victor Vasiliev <vasilievvict1@yandex.ru>
2022-12-05 14:58:00 +0300

    Changed scene switch triggers

--- Assets/Scripts/Objects/Triggers/CutsceneTrigger.cs
+++ Assets/Scripts/Objects/Triggers/CutsceneTrigger.cs
@@ -6,36 +6,12 @@ using UnityEngine.SceneManagement;
 public class CutsceneTrigger : MonoBehaviour
 {
     [SerializeField] private PlayableDirector cutscene;
-    [SerializeField] private string nextScene;
-    [SerializeField] private bool disableCrossfadeOut;
-    public Animator crossfade;
-    private bool activated;
-    private DialogueManager dialogueManager;
-
-    private void Update()
-    {
-        if (!dialogueManager) dialogueManager = DialogueManager.GetInstance();
-        if (activated && !dialogueManager.DialoguePlaying)
-        {
-            activated = false;
-            if (cutscene) cutscene.Play();
-            StartCoroutine(SwitchScene());
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) activated = true;
-    }
-
-    private IEnumerator SwitchScene()
-    {
-        if (cutscene) yield return new WaitForSeconds((float)cutscene.duration);
-        if (nextScene.Length != 0)
+        if (other.CompareTag("Player"))
         {
-            if (!disableCrossfadeOut) crossfade.SetTrigger("Crossfade");
-            yield return new WaitForSeconds(1.0f);
-            SceneManager.LoadScene(nextScene);
+            cutscene.Play();
         }
     }
 }
---
+++ Assets/Scripts/Objects/Triggers/SceneSwitchTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using GUI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Objects.Triggers
+{
+    public class SceneSwitchTrigger : MonoBehaviour
+    {
+        [SerializeField] private string sceneId;
+        [SerializeField] private SceneCrossfader crossfadeAnimator;
+        [SerializeField] private bool disableCrossfade;
+
+        private delegate void OnSceneCrossfade();
+
+        private OnSceneCrossfade OnSceneCrossfadeEvent;
+        
+        private void Start()
+        {
+            if (crossfadeAnimator != null)
+            {
+                OnSceneCrossfadeEvent += crossfadeAnimator.StartAnimation;
+            }
+        }
+
+        private IEnumerator SwitchScene(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            if (sceneId.Length != 0)
+            {
+                if (!disableCrossfade)
+                {
+                    OnSceneCrossfadeEvent?.Invoke();
+                    yield return new WaitForSeconds(1.0f);
+                }
+                SceneManager.LoadScene(sceneId);
+            }
+        }
+    }
+}
